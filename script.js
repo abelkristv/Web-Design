308ab2183a928e8cc5e81532125f3c4b431bd70c
abelkristv <abel.kristanto1@gmail.com>
2023-07-30 17:18:42 +0700

    initial commit (home page selesai)

---
+++ script.js
@@ -0,0 +1,148 @@
+let button1 = document.getElementById('button1');
+let button1cond = true;
+button1.addEventListener('click', function() {
+    if (button1cond) {
+        button1cond = false;
+        let image1 = document.getElementById('image1');
+        image1.style.display = "none";
+        let story1 = document.getElementById('story1');
+        story1.style.backgroundSize = "100% 60%";
+        button1.innerHTML = "Show image";
+        return;
+    }
+    button1cond = true;
+    let image1 = document.getElementById('image1');
+    image1.style.display = "block";
+    let story1 = document.getElementById('story1');
+    story1.style.backgroundSize = "100% 70%";
+    button1.innerHTML = "Hide image";
+    return;
+})
+
+let button2 = document.getElementById('button2');
+let button2cond = true;
+button2.addEventListener('click', function() {
+    if (button2cond) {
+        button2cond = false;
+        let image2 = document.getElementById('image2');
+        image2.style.display = "none";
+        let story2 = document.getElementById('story2');
+        story2.style.backgroundSize = "100% 60%";
+        button2.innerHTML = "Show image";
+        return;
+    }
+    button2cond = true;
+    let image2 = document.getElementById('image2');
+    image2.style.display = "block";
+    let story2 = document.getElementById('story2');
+    story2.style.backgroundSize = "100% 70%";
+    button2.innerHTML = "Hide image";
+    return;
+})
+
+let button3 = document.getElementById('button3');
+let button3cond = true;
+button3.addEventListener('click', function() {
+    if (button3cond) {
+        button3cond = false;
+        let image3 = document.getElementById('image3');
+        image3.style.display = "none";
+        let story3 = document.getElementById('story3');
+        story3.style.backgroundSize = "100% 60%";
+        button3.innerHTML = "Show image";
+        return;
+    }
+    button3cond = true;
+    let image3 = document.getElementById('image3');
+    image3.style.display = "block";
+    let story3 = document.getElementById('story3');
+    story3.style.backgroundSize = "100% 70%";
+    button3.innerHTML = "Hide image";
+    return;
+})
+
+
+let button4 = document.getElementById('button4');
+let button4cond = true;
+button4.addEventListener('click', function() {
+    if (button4cond) {
+        button4cond = false;
+        let image4 = document.getElementById('image4');
+        image4.style.display = "none";
+        let story4 = document.getElementById('story4');
+        story4.style.backgroundSize = "100% 60%";
+        button4.innerHTML = "Show image";
+        return;
+    }
+    button4cond = true;
+    let image4 = document.getElementById('image4');
+    image4.style.display = "block";
+    let story4 = document.getElementById('story4');
+    story4.style.backgroundSize = "100% 70%";
+    button4.innerHTML = "Hide image";
+    return;
+})
+
+let button5 = document.getElementById('button5');
+let button5cond = true;
+button5.addEventListener('click', function() {
+    if (button5cond) {
+        button5cond = false;
+        let image5 = document.getElementById('image5');
+        image5.style.display = "none";
+        let story5 = document.getElementById('story5');
+        story5.style.backgroundSize = "100% 60%";
+        button5.innerHTML = "Show image";
+        return;
+    }
+    button5cond = true;
+    let image5 = document.getElementById('image5');
+    image5.style.display = "block";
+    let story5 = document.getElementById('story5');
+    story5.style.backgroundSize = "100% 70%";
+    button5.innerHTML = "Hide image";
+    return;
+})
+
+
+let button6 = document.getElementById('button6');
+let button6cond = true;
+button6.addEventListener('click', function() {
+    if (button6cond) {
+        button6cond = false;
+        let image6 = document.getElementById('image6');
+        image6.style.display = "none";
+        let story6 = document.getElementById('story6');
+        story6.style.backgroundSize = "100% 60%";
+        button6.innerHTML = "Show image";
+        return;
+    }
+    button6cond = true;
+    let image6 = document.getElementById('image6');
+    image6.style.display = "block";
+    let story6 = document.getElementById('story6');
+    story6.style.backgroundSize = "100% 70%";
+    button6.innerHTML = "Hide image";
+    return;
+})
+
+let button7 = document.getElementById('button7');
+let button7cond = true;
+button7.addEventListener('click', function() {
+    if (button7cond) {
+        button7cond = false;
+        let image7 = document.getElementById('image7');
+        image7.style.display = "none";
+        let story7 = document.getElementById('story7');
+        story7.style.backgroundSize = "100% 60%";
+        button7.innerHTML = "Show image";
+        return;
+    }
+    button7cond = true;
+    let image7 = document.getElementById('image7');
+    image7.style.display = "block";
+    let story7 = document.getElementById('story7');
+    story7.style.backgroundSize = "100% 70%";
+    button7.innerHTML = "Hide image";
+    return;
+})
